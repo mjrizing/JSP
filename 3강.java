db0728e4657a7891fafc094e37500fe7341282b1
MinjaeJung <77330116+mjrizing@users.noreply.github.com>
2021-05-27 12:26:49 +0900

    Update 3강.java

--- 3강.java
+++ 3강.java
@@ -78,5 +78,63 @@ include 디렉티브 태그는 JSP 페이지 어디에서든 선언 가능
 <taglib 디렉티브 태그>
 현재 JSP 페이지에 표현 언어, JSTL, 사용자 정의 태그(custom tag) 등 태그 라이브러리를 설정하는 태그
 
+// Welcome.jsp
+<%@ page contentType="text/html; charset=utf-8"%>
+<%@ page import="java.util.Date"%>
+<html>
+<head>
+<link rel="stylesheet"	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<title>Welcome</title>
+</head>
+<body>
+	<%@ include file="menu.jsp"%>
+	<%!String greeting = "웹 쇼핑몰에 오신 것을 환영합니다";
+	String tagline = "Welcome to Web Market!";%>
+	<div class="jumbotron">
+		<div class="container">
+			<h1 class="display-3">
+				<%=greeting%>
+			</h1>
+		</div>
+	</div>	
+	<div class="container">
+		<div class="text-center">
+			<h3>
+				<%=tagline%>
+			</h3>
+			<%
+				Date day = new java.util.Date();
+				String am_pm;
+				int hour = day.getHours();
+				int minute = day.getMinutes();
+				int second = day.getSeconds();
+				if (hour / 12 == 0) {
+					am_pm = "AM";
+				} else {
+					am_pm = "PM";
+					hour = hour - 12;
+				}
+				String CT = hour + ":" + minute + ":" + second + " " + am_pm;
+				out.println("현재 접속  시각: " + CT + "\n");
+			%>
+		</div>
+		<hr>
+	</div>	
+	<%@ include file="footer.jsp"%>
+</body>
+</html>
 
 
+// footer.jsp
+<footer class="container">
+	<p>&copy; WebMarket</p>
+</footer>
+
+// menu.jsp
+<nav class="navbar navbar-expand  navbar-dark bg-dark">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="./welcome.jsp">Home</a>
+		</div>
+	</div>
+</nav>
